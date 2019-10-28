e5c30ac9f861e2fec45642d68a6de8278afccfe1
Spencer Butler <github@crooked.app>
2019-10-28 18:13:02 -0500

    finalizing helper script

--- bin/tmux-init.sh
+++ bin/tmux-init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Initialize the tmuxeses
-# Spencer Butler <spencerb@honeycomb.net>
+# Spencer's tmux-tweaks install helper
+# Spencer Butler <git@crooked.app>
 
-giturl='https://github.com'
 declare -A repos
+giturl='https://github.com'
 repos[spencerbutler/tmux-tweaks]="$HOME/tmux-tweaks"
 repos[tmux-plugins/tpm]="$HOME/.tmux/plugins/tpm"
 
@@ -13,14 +13,6 @@ have_tmux() {
 
 have_git() {
     command -v git 1>/dev/null
-}
-
-have_dirs() {
-    [ -d ${repos[$1]} ]
-}
-
-get_gits() {
-    git clone ${giturl}/${1}.git ${repos[$1]} || { echo Could not get ${giturl}/${1}.git; }
 }
 
 get_linux_ver() {
@@ -33,27 +25,78 @@ get_linux_ver() {
     fi
 }
 
-linux() {
-  if [ ! have_tmux ]; then
-      echo "You don't have the tmux, installing."
-      if [ $linux_name == centos  ]; then
-        if [ $linux_maj -lt 8 ]; then
-          echo "CentOS $linux_maj is old and comes with tmux 1.8"
-          echo "You should install the IUS repo"
-          echo "SHELL$: sudo yum install https://repo.ius.io/ius-release-el$linux_maj.rpm"
-          echo "SHELL$: sudo yum install tmux2"
-          echo 
-          echo "Installing the tmux for your release."
-          echo "Some stuff is not going to work."
-          sudo yum install tmux || { echo "I could not install tmux"; exit; }
-        else
-          sudo yum install tmux || { echo "I could not install tmux"; exit; }
-        fi
-      elif [ $linux_name == debian ]; then
-        sudo apt install tmux
+install_tmux() {
+  echo "You don't have the tmux, installing."
+  if [ $linux_name == centos  ]; then
+    if [ $linux_maj -lt 8 ]; then
+      echo "CentOS $linux_maj is old and comes with tmux 1.8"
+      echo "You should install the IUS repo"
+      echo "SHELL$: sudo yum install https://repo.ius.io/ius-release-el$linux_maj.rpm"
+      echo "SHELL$: sudo yum install tmux2"
+      echo 
+      echo "Installing the tmux for your release."
+      echo "Some stuff is not going to work."
+      sudo yum install tmux || { echo "I couldn't install tmux"; exit; }
+    else
+      sudo yum install tmux || { echo "I couldn't install tmux"; exit; }
+    fi
+
+  elif [ $linux_name == debian ]; then
+    sudo apt install tmux || { echo "I couldn't install tmux"; exit; }
+
+  elif [ $osname == fbsd ]; then
+    sudo pkg install tmux || { echo "I couldn't install tmux"; exit; }
+
+  elif [ $osname == obsd ]; then
+    echo "tmux is in OpenBSD base, not sure what's going on here."
+    exit 1
+
+  else
+    echo "Use your package managing skills to install tmux."
+  fi
+}
+
+install_git() {
+  echo "You have no git! Let's try and install it."
+  if [ $linux_name == centos ]; then
+    sudo yum install git || { echo "I tried..."; exit; }
+
+  elif [ $linux_name == debian ]; then
+    sudo apt install git || { echo "I tried..."; exit; }
+
+  elif [ $osname == fbsd ]; then
+    sudo pkg install git || { echo "I tried..."; exit; }
+
+  elif [ $osname == obsd ]; then
+    pkginfo=$(sudo pkg_info -Q git)
+    if [ -z $pkginfo ]; then
+      echo "I can't find git in your PKG_PATH. Let's try ftp.openbsd.org"
+      echo "exporting PKG_PATH=$PKG_PATH:http://ftp.openbsd.org/pub/OpenBSD/%c/packages/%a/"
+      export PKG_PATH=$PKG_PATH:http://ftp.openbsd.org/pub/OpenBSD/%c/packages/%a/ 
+      sudo pkg_add git
+      if command -v git 1>/dev/null; then
+        echo "Git installed. Now let's get tmux working."
       else
-        echo "Use your package manager to install tmux."
+        echo "You'll need to install git to proceed"; exit
       fi
+    fi
+  fi
+  echo "Now you have the gits!!"
+}
+
+have_dirs() {
+    [ -d ${repos[$1]} ]
+}
+
+get_gits() {
+    git clone ${giturl}/${1}.git ${repos[$1]} || { echo Could not get ${giturl}/${1}.git; }
+}
+
+main() {
+  if [ ! have_tmux ]; then
+    install_tmux
+  elif [ ! have_git ]; then
+    install_git
   fi
 
   if have_git; then
@@ -76,62 +119,17 @@ linux() {
         echo "You alread have a $HOME/.tmux.conf! What to do, Yo?"
     fi
 
-    echo You are ready to tmux shit up.
+    echo -e "\nYou are ready to tmux shit up."
   else
-     echo "You have no git!"
-     if [ $linux_name == centos ]; then
-        sudo yum install git || { echo "I tried..."; exit; }
-     elif [ $linux_name == debian ]; then
-        sudo apt install git || { echo "I tried..."; exit; }
-     fi
-     echo "Now you have the gits!!"
-     linux
-  fi
-}
-
-fbsd() {
-  if [ ! have_tmux ]; then
-      echo "You don't have the tmux, installing."
-      sudo pkg add tmux || { echo "I could not install tmux"; exit; }
-  fi
-
-  if have_git; then
-    linux
-  else
-    echo "You have no git!"
-    sudo pkg install git || { echo "Could not install git."; exit; }
-    echo "Now you have the gits!!"
-    linux
-  fi
-}
-
-obsd() {
-  # tmux is a part of OpenBSD base
-  if have_git; then
-      linux
-  else
-    echo "You have no git!"
-    echo "I'll try to install"
-    pkginfo=$(sudo pkg_info -Q git)
-    if [ -z $pkginfo ]; then
-      echo "I can't find git in your PKG_PATH. Let's try ftp.openbsd.org"
-      echo "PKG_PATH = $PKG_PATH"
-      export PKG_PATH=$PKG_PATH:http://ftp.openbsd.org/pub/OpenBSD/%c/packages/%a/ 
-      sudo pkg_add git
-      if command -v git 1>/dev/null; then
-        echo "Git installed. Now let's get tmux working."
-        linux
-      else
-        echo "You'll need to install git to proceed"; exit
-      fi
-    fi
+    echo "We already tried to install git and it didn't work out. Please fix something."
   fi
 }
 
 case $(uname -s) in
-  Linux  ) get_linux_ver; linux ;;
-  FreeBSD) fbsd ;;
-  OpenBSD) obsd ;;
-  *      ) { echo "$(uname -s) not supported."; exit; }
+  Linux  ) osname=linux get_linux_ver ;;
+  FreeBSD) osname=fbsd ;;
+  OpenBSD) osname=obsd ;;
+  *      ) { echo "So sorry, $(uname -s) not supported."; exit; }
 esac
 
+main
